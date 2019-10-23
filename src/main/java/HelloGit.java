101b151849a2248efb8745711b7fee3b77ac8b70
chennengyuan <1870680926@qq.com>
2019-10-23 14:01:38 +0800

    master add

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -4,11 +4,5 @@
 public class HelloGit {
     public static void main(String[] args){
         System.out.println("云上第一句");
-        System.out.println("dev第二句");
-        System.out.println("主分支第三句");
-        System.out.println("云上第四句");
-        System.out.println("云上第六句");
-        System.out.println("第七局");
-        System.out.println("第八局");
     }
 }
