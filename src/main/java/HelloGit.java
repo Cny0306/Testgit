ca3f3047794a691e363e8564ff1874d51289bb46
Cny0306 <53330161+Cny0306@users.noreply.github.com>
2019-10-23 11:55:43 +0800

    Update HelloGit.java

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -6,5 +6,6 @@ public class HelloGit {
         System.out.println("云上第一句");
         System.out.println("dev第二句");
         System.out.println("主分支第三句");
+         System.out.println("云上第四句");
     }
 }
